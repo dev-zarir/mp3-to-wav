7b408d0eb8238092ac2f00524f9998d555849538
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2023-01-10 14:34:06 +0600

    Edit app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, jsonify
 from urllib.parse import unquote
 from base64 import b64decode
 import subprocess, random
@@ -8,7 +8,9 @@ import os
 app=Flask(__name__)
 app.config['SECRET_KEY']='somerandomkey'
 
-os.mkdir('static')
+try:
+    os.mkdir('static')
+except: pass
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -21,7 +23,7 @@ def home():
         mp3.write(data)
         mp3.close()
     subprocess.call(['ffmpeg', '-y', '-i', 'static/'+name+'.mp3', 'static/'+name+'.wav'], stdout = subprocess.DEVNULL, stderr = subprocess.STDOUT)
-    return redirect('/static/'+name+'.wav')
+    return jsonify({'link': request.base_url+'/static/'+name+'.wav'.replace('//','/')})
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
