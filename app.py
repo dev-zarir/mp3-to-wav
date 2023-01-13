ff9f2e254c8ba0b741b446bb3df4a0fc194791fe
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2023-01-13 16:44:55 +0600

    Add delete function

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from base64 import b64decode
 import subprocess, random
 from string import ascii_letters
 import os
+from time import sleep
+from threading import Thread
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='somerandomkey'
@@ -11,6 +13,11 @@ app.config['SECRET_KEY']='somerandomkey'
 try:
     os.mkdir('static')
 except: pass
+
+def del_func(mp3_path, wav_path):
+    sleep(60*10)
+    os.remove(mp3_path)
+    os.remove(wav_path)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -23,6 +30,9 @@ def home():
         mp3.write(data)
         mp3.close()
     subprocess.call(['ffmpeg', '-y', '-i', 'static/'+name+'.mp3', 'static/'+name+'.wav'], stdout = subprocess.DEVNULL, stderr = subprocess.STDOUT)
+    t=Thread(target=lambda: del_func('static/'+name+'.mp3', 'static/'+name+'.wav'))
+    t.setDaemon(True)
+    t.start()
     return jsonify({'link': (request.base_url+'/static/'+name+'.wav').replace('//','/').replace(':/','://')})
 
 if __name__=='__main__':
